8e03cf46ac60746ec249dec6db8fe163191c77b4
Deepa Dhurka <deepa.dhurka@gmail.com>
2013-06-12 11:12:48 -0700

    Cleaned up design and updated header files

--- include/cc_net_conn.h
+++ include/cc_net_conn.h
@@ -4,91 +4,67 @@
 #ifndef CC_NET_CONN_H
 #define CC_NET_CONN_H
 
+typedef enum of_drv_type_ {
+    CLIENT = 0,
+    SERVER,
+    MAX_OF_DRV_TYPE
+} of_drv_type_e;
+
 typedef enum L4_type_ {
     /* used for array index */
     TCP = 0,
-//    TLS,
-//    UDP,
-//    DTLS,
+    TLS,
+    UDP,
+    DTLS,
     /* additional types here */ 
     MAX_L4_TYPE
 } L4_type_e;
-
-typedef enum conn_type_ {
-    CLIENT = 0,
-    SERVER,
-    MAX_CONN_TYPE
-} conn_type_e;
 
 typedef enum addr_type_ {
     IPV4,
     IPV6
 } addr_type_e;
 
-typedef struct client_svcs_ {
+typdef struct ipaddr_v4v6_ {
+    addr_type_e  ipaddr_type;
+    union {
+        uint32_t ipv4_addr;
+        uint32_t ipv6_addr[4];
+    } u;
+} ipaddr_v4v6_t;
+
+typedef struct net_svcs_ {
+    int (*setup_conn)();
     int (*open_conn)(addr_type_e ip_ver, int *sock_fd);
     void (*close_conn)(int *sock_fd);
     int (*connect_conn)(int sock_fd);
-    int (*recv_data)();
-    int (*send_data)();
-} client_svcs_t;
-
-typedef struct server_svcs_ {
-    int (*open_conn)(addr_type_e ip_ver, int *sock_fd);
-    void (*close_conn)(int *sock_fd);
     int (*listen_conn)();
     int (*accept_conn)();
     int (*recv_data)();
     int (*send_data)();
-} server_svcs_t;
+} net_svcs_t;
 
-typedef struct conn_key_ {
-    int sockfd;
-} conn_key_t;
+/* sample code to make this work
+   of_drv_type_e s_type = CLIENT;
+   L4_type_e l4_type = UDP;
+   net_svcs_t *my_svcs;
+   
+   svcs_t CALLBACKS[MAX_SVCS_TYPE][MAX_L4_TYPE] = {
+   {
+   {tcp_client_open, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+   {tls_client_open, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+   {udp_client_open, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+   {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL}},
+   {
+   {tcp_server_open, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+   {tls_server_open, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+   {udp_server_open, NULL, NULL, NULL, NULL, NULL, NULL, NULL},
+   {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL}}
+   };
 
-
-/* A single server connection with multiple threads
- * to manage the r/w sockets
- */
-typedef struct server_conn_ {
-    conn_key_t      key;
-    server_svcs_t   *svcs;
-
-    addr_type_e   ip_ver;
-    int           listen_sockfd;
-
-    uint_32       rw_socket_thread_count;
-    
-    /*list of rw_sock_fd */
-    GList         *rw_sockfd_list;
-            
-    /* thread information */
-    GList         *rw_thread_list;
-    
-    /* state */
-
-    /* stats */
-
-} server_conn;
-
-/* One client connection per r/w socket for which a thread
- * will be spawned
- */
-typedef struct client_conn_ {
-    conn_key_t        key;
-    client_svcs_t    *svcs;
-
-    addr_type_e   ip_ver;
-
-    int           rw_sockfd;
-    
-    /* thread information */
-    
-
-    /* state */
-    
-    /* stats */
-} client_conn;
-
-
+   my_svcs = &(CALLBACKS[s_type][l4_type]);
+   
+   my_svcs->open();
+*/
+   
 #endif //CC_NET_CONN_H
---
+++ include/cc_of_global.h
@@ -0,0 +1,86 @@
+/*-----------------------------------------------------------------------------*/
+/* Copyright: CodeChix Bay Area Chapter 2013                                   */
+/*-----------------------------------------------------------------------------*/
+#ifndef CC_OF_GLOBAL_H
+#define CC_OF_GLOBAL_H
+
+#include <glib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <pthread.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "cc_net_conn.h"
+#include "cc_log.h"
+
+typedef enum of_dev_type_ {
+    SWITCH = 0,
+    CONTROLLER,
+    MAX_OF_DEV_TYPE
+} of_dev_type_e;
+
+typdef struct cc_ofchannel_key_ {
+    uint64_t  dp_id;
+    uint8_t   aux_id;
+} cc_of_channel_key_t;
+
+typedef enum cc_ofchannel_state_ {
+    CC_OF_CHANNEL_DOWN = 0,
+    CC_OF_CHANNEL_UP
+} cc_ofchannel_state_e;
+
+
+/* node in ofchannel_htbl */
+typdef struct cc_ofchannel_info_ {
+    cc_ofchannel_key_t   key;
+    cc_ofchannel_state_e state;
+    GThread              *thread_p;
+    L4_type_e            layer4_proto;
+    int                  rw_sockfd;
+    //RD pipe to thread
+    //WR pipe to thread
+    //synchronization vars
+
+    /* stats */
+
+} cc_ofchannel_info_t;
+
+
+typedef struct cc_ofdev_key_ {
+    ipaddr_v4v6_t  controller_ip_addr;
+    ipaddr_v4v6_t  switch_ip_addr[]; /*UNUSED for dev type CONTROLLER */
+} cc_ofdev_key_t
+
+/* node in ofdev_htbl */
+typedef struct cc_ofdev_info_ {
+    cc_ofdev_key_t key;
+    uint16_t       controller_L4_ports[MAX_L4_TYPE];
+    uint16_t       switch_L4_port[MAX_L4_TYPE];
+
+    uint32_t       count_channels;
+    GHashTable     *ofchannel_htbl;
+
+    int            main_sockfd;
+} cc_ofdev_info_t;
+
+
+typedef struct cc_of_global_ {
+    /* device type could be controller or switch */
+    of_drv_type_e     ofdrv_type;
+
+    /* controller: spawns multiple IP address endpoints
+     *             for the same controller
+     *             (multiple instances of same controller)
+     * switch:
+     */
+    GHashTable       *ofdev_htbl;
+
+    net_svcs_t       NET_SVCS[MAX_OF_DRV_TYPE][MAX_L4_TYPE];
+    
+} cc_of_global_t;
+
+#endif //CC_OF_GLOBAL_H
